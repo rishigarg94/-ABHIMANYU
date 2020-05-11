0d69552b11b8ff78f6fdc91e643271eedde3dc7e
Rishi Garg <58929857+rishigarg94@users.noreply.github.com>
2020-05-11 21:58:35 +0530

    login and registration page full backend and databse created

---
+++ abhimanyu/registerphp/index.php
@@ -0,0 +1,20 @@
+<?php
+//include auth.php file on all secure pages
+include("auth.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Welcome Home</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="form">
+<p>Welcome <?php echo $_SESSION['username']; ?>!</p>
+<p>This is secure area.</p>
+<p><a href="dashboard.php">Dashboard</a></p>
+<a href="logout.php">Logout</a>
+</div>
+</body>
+</html>
