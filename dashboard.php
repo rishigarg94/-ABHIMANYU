49c1ad2a4ff358d6990ae012508708ab5b13d1a9
Rishi Garg <58929857+rishigarg94@users.noreply.github.com>
2020-05-11 22:03:27 +0530

    uploaded login and registration page with full backend and database created on master repo.

---
+++ dashboard.php
@@ -0,0 +1,20 @@
+<?php
+require('db.php');
+include("auth.php");
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Dashboard - Secured Page</title>
+<link rel="stylesheet" href="css/style.css" />
+</head>
+<body>
+<div class="form">
+<p>Dashboard</p>
+<p>This is another secured page.</p>
+<p><a href="index.php">Home</a></p>
+<a href="logout.php">Logout</a>
+</div>
+</body>
+</html>
