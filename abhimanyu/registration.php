46b0ce6ec8fee739460f2892862069a1662b1ba3
Rishi Garg <58929857+rishigarg94@users.noreply.github.com>
2020-05-11 23:14:15 +0530

    fix error on reegistration php

--- abhimanyu/registration.php
+++ abhimanyu/registration.php
@@ -3,7 +3,7 @@
 <head>
 <meta charset="utf-8">
 <title>Registration</title>
-<link rel="stylesheet" href="css/style.css" />
+<link rel="stylesheet" href="css/stylelogin.css" />
 </head>
 <body>
 <?php
