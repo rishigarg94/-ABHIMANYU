57193faf9444554a5a54a75fdc4a8c6b10e07033
Rishi Garg <58929857+rishigarg94@users.noreply.github.com>
2020-05-12 12:34:55 +0530

    change the hostname

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost","root","","register");
+$con = mysqli_connect("githost","git","","register");
 // Check connection
 if (mysqli_connect_errno())
   {
