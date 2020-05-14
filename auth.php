9140d266eb1ddaab168de9c7b0b5bae1ed801737
Rishi Garg <58929857+rishigarg94@users.noreply.github.com>
2020-05-15 01:07:53 +0530

    Update auth.php

--- auth.php
+++ auth.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 //
-$con = mysqli_connect("localhost","root","","register");
+$con = mysqli_connect("sql204.epizy.com","epiz_25759997","TKKRT61A0T75","epiz_25759997_register");
 //$res = mysqli_query($con, "SELECT * FROM 'users' WHERE username='$username'");
      //$_SESSION1 = mysqli_fetch_array($res,MYSQLI_NUM);
      //$res = mysqli_query($con, "SELECT regis FROM 'users' WHERE username='$username'");
