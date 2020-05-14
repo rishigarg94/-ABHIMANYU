f4c8c97a2d190e5bae4e59e6f6b9891c11e9520b
Rishi Garg <58929857+rishigarg94@users.noreply.github.com>
2020-05-15 01:09:55 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
 // Enter your Host, username, password, database below.
 // I left password empty because i do not set password on localhost.
-$con = mysqli_connect("localhost","root","","register");
+$con = mysqli_connect("sql204.epizy.com","epiz_25759997","TKKRT61A0T75","epiz_25759997_register");
 // Check connection
 if (mysqli_connect_errno())
   {
